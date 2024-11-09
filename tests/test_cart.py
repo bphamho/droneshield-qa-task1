7f9fcd177afd77c31649f372ff7696fbdf0d91c7
Benjamin PH <benjamin.phamho@gmail.com>
2024-11-09 17:50:20 +1100

    added test cases for cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -30,11 +30,13 @@ def test_addcart():
     driver.find_element(By.ID, "remove-sauce-labs-backpack").click()
 
     # Verify item has been added
-    cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
-    assert cart == "0"
-    driver.quit()
+    try:
+        cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+        raise Exception("Still an item in the cart")
+    except NoSuchElementException:
+        pass
 
-# Test adding all items on the page to the cart
+# Test adding all items on the page to the cart and then removing
 def test_addcartall():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
@@ -71,6 +73,45 @@ def test_addcartall():
         except NoSuchElementException:
             if (index != len(remove_from_cart_btn) - 1):
                 raise Exception("Items added but cart badge not matching")
-            pass
             
     driver.quit()
+
+# Test removing items from cart
+def test_remove_from_cart():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    # Log into user account
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    # Find all add to cart buttons on page
+    add_to_cart_btn = driver.find_elements(By.CSS_SELECTOR, 'button[class*="btn_inventory"]')
+    
+    # Press each button one by one and verify cart
+    for index, btn in enumerate(add_to_cart_btn):
+        btn.click()
+        
+        # Verify cart is correct
+        cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+        assert cart == str(index + 1)
+    
+    # Click on cart top right
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+    
+    # Remove each item one by one and verify cart
+    
+    remove_btn = driver.find_elements(By.CSS_SELECTOR, 'button[class*="cart_button"]')
+    
+    for index, btn in enumerate(remove_btn):
+        btn.click()
+        sleep(1)
+        
+        # Verify cart is correct
+        try:
+            cart = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+            assert cart == str(len(remove_btn) - index - 1)
+        except NoSuchElementException:
+            if (index != len(remove_btn) - 1):
+                raise Exception("Items added but cart badge not matching")
